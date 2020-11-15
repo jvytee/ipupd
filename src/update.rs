60e65f43a9c06f6c7927cde2db6e3e504c774ba6
Julian Theis <julian.theis@posteo.de>
2020-11-15 19:49:49 +0100

    Try to handle errors during update

--- src/update.rs
+++ src/update.rs
@@ -1,9 +1,14 @@
-pub fn update(url: &str, basic_auth: Option<(&str, &str)>) {
+pub fn update(url: &str, basic_auth: Option<(&str, &str)>) -> Result<String>{
     let mut request = ureq::get(url);
 
     if let Some(user, password) = basic_auth {
         request = request.auth(user, password)
     }
 
-    let _response = request.call();
+    let response = request.call();
+    return if let Some(error) = response.synthetic_error() {
+        Err(error)
+    } else {
+        Ok(response.into_string().unwrap_or(String::new()))
+    }
 }
