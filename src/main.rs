d0331b0477bc604addeaa4f0588d5541d28d1c15
Julian Theis <julian.theis@posteo.de>
2020-11-15 13:03:10 +0100

    Use interface ip retrieval function

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,6 @@ use getopts::Options;
 use ip_addrs::IpAddrs;
 use std::env;
 use std::error::Error;
-use std::net::{
-    SocketAddr::{V4, V6},
-    ToSocketAddrs
-};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut opts = Options::new();
@@ -16,17 +12,24 @@ fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let matches = opts.parse(&args).expect("Could not parse arguments");
 
-    if matches.free.len() < 2 || matches.opt_present("h") {
-        let usage = format!("Usage: {} DOMAIN", args[0]);
+    if matches.free.len() < 3 || matches.opt_present("h") {
+        let usage = format!("Usage: {} INTERFACE DOMAIN", args[0]);
         println!("{}", opts.usage(&usage));
         return Ok(());
     }
 
-    let domain = &matches.free[1];
-    let ip_addrs = IpAddrs::from_domain(domain).expect(&format!("Could not resolve {}", domain));
+    let interface = &matches.free[1];
+    let interface_ips = IpAddrs::from_interface(interface).expect(&format!("Could not find interface {}", interface));
+    println!("Interface: {}\n\
+              IPv4: {}\n\
+              IPv6: {}\n", interface, interface_ips.v4.unwrap_or("None".to_string()), interface_ips.v6.unwrap_or("None".to_string()));
+
+
+    let domain = &matches.free[2];
+    let domain_ips = IpAddrs::from_domain(domain).expect(&format!("Could not resolve {}", domain));
     println!("Domain: {}\n\
               IPv4: {}\n\
-              IPv6: {}", domain, ip_addrs.v4.unwrap_or("None".to_string()), ip_addrs.v6.unwrap_or("None".to_string()));
+              IPv6: {}", domain, domain_ips.v4.unwrap_or("None".to_string()), domain_ips.v6.unwrap_or("None".to_string()));
 
     Ok(())
 }
