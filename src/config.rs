c3a9b9fae27ab04dd24bfa30c919d49516a6c7a4
Julian Theis <julian.theis@posteo.de>
2020-11-18 20:16:24 +0100

    Add method to read config from toml file

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,11 @@
 use serde::Deserialize;
+use std::fs::File;
+use std::io::{
+    prelude::*,
+    Error,
+    ErrorKind,
+    Result
+};
 
 #[derive(Deserialize)]
 struct Config {
@@ -7,4 +14,18 @@ struct Config {
     url: String,
     user: Option<String>,
     password: Option<String>,
+}
+
+impl Config {
+    fn from_file(filename: &str) -> Result<Config> {
+        let mut file = File::open(filename)?;
+        let mut content = String::new();
+
+        return if let Ok(_bytes) = file.read_to_string(&mut content) {
+            let config: Config = toml::from_str(&content)?;
+            Ok(config)
+        } else {
+            Err(Error::new(ErrorKind::InvalidData, "Could not parse configuration data"))
+        }
+    }
 }
