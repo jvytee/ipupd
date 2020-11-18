855a719746cc0fe2f0fe5572e24a51bbf6a90244
Julian Theis <julian.theis@posteo.de>
2020-11-18 21:31:41 +0100

    Use config file for parameterization

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod config;
 mod ipaddrs;
 mod update;
 
+use config::Config;
 use getopts::Options;
 use ipaddrs::IpAddrs;
 use std::env;
@@ -9,29 +10,38 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut opts = Options::new();
-    opts.optflag("h", "help", "show help");
+    opts.optflag("h", "help", "show help and exit");
+    opts.optopt("c", "config", "configuration file", "FILE");
 
     let args: Vec<String> = env::args().collect();
     let matches = opts.parse(&args).expect("Could not parse arguments");
 
-    if matches.free.len() < 3 || matches.opt_present("h") {
-        let usage = format!("Usage: {} INTERFACE DOMAIN", args[0]);
+    if matches.opt_present("h") {
+        let usage = format!("Usage: {}", args[0]);
         println!("{}", opts.usage(&usage));
         return Ok(());
     }
 
-    let interface = &matches.free[1];
-    let interface_ips = IpAddrs::from_interface(interface).expect(&format!("Could not inspect {}", interface));
-    println!("Interface: {}\n\
+    let config_file = matches.opt_str("c").unwrap_or("config.toml".to_string());
+
+    match Config::from_file(&config_file) {
+        Ok(config) => {
+            let interface = &config.interface;
+            let interface_ips = IpAddrs::from_interface(interface).expect(&format!("Could not inspect {}", interface));
+            println!("Interface: {}\n\
               IPv4: {}\n\
               IPv6: {}\n", interface, interface_ips.v4.unwrap_or("None".to_string()), interface_ips.v6.unwrap_or("None".to_string()));
 
-
-    let domain = &matches.free[2];
-    let domain_ips = IpAddrs::from_domain(domain).expect(&format!("Could not resolve {}", domain));
-    println!("Domain: {}\n\
+            let domain = &config.domain;
+            let domain_ips = IpAddrs::from_domain(domain).expect(&format!("Could not resolve {}", domain));
+            println!("Domain: {}\n\
               IPv4: {}\n\
               IPv6: {}", domain, domain_ips.v4.unwrap_or("None".to_string()), domain_ips.v6.unwrap_or("None".to_string()));
+        },
+        Err(error) => {
+            println!("Could not parse config file {}: {}", &config_file, error);
+        }
+    };
 
     Ok(())
 }
