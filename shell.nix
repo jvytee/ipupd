fe86781e347f5624f527fbe36295aa49c4b23f52
Julian Theis <julian.theis@posteo.de>
2021-08-22 20:39:13 +0200

    Set up rust via mozilla overlay instead of rustup

--- shell.nix
+++ shell.nix
@@ -1,14 +1,17 @@
-with import <nixpkgs> {};
-
-mkShell {
-  nativeBuildInputs = [
-    pkgconfig
-    rust-analyzer
-    rustup
-  ];
-
-  buildInputs = [
-    cacert
-    openssl
-  ];
-}
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+  with nixpkgs;
+  stdenv.mkDerivation {
+    name = "nix-shell_ipupd";
+    buildInputs = [
+      nixpkgs.latest.rustChannels.stable.rust
+      cacert
+      openssl
+    ];
+    nativeBuildInputs = [
+      pkgconfig
+      rust-analyzer
+    ];
+  }
