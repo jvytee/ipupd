0e2cff7a6b3ae50f0bea04a2b4fd36f876992761
Julian Theis <julian.theis@posteo.de>
2023-03-05 22:09:18 +0100

    Create basic nix flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+  in
+  {
+    devShells.${system}.default = with import nixpkgs { inherit system; }; mkShell {
+      buildInputs = [
+        cargo
+        rust-analyzer
+        rustc
+      ];
+    };
+  };
+}
