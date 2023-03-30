a684a93c34cb08e53fc9f9e5e1109d8565b946d1
Julian Theis <julian.theis@posteo.de>
2023-03-30 18:06:59 +0200

    Make devShells system agnostic

--- flake.nix
+++ flake.nix
@@ -1,15 +1,9 @@
 {
   outputs = { self, nixpkgs }:
   let
-    system = "x86_64-linux";
-    # crossSystem = { config = "aarch64-unknown-linux-gnu"; };
-    # crossSystem = { config = "aarch64-unknown-linux-musl"; };
-    crossSystem = { config = "x86_64-unknown-linux-musl"; };
-    nixpkgsCross = import nixpkgs { inherit system crossSystem; };
-  in
-  with nixpkgsCross;
-  {
-    devShells.${system}.ipupdAarch64 = mkShell {
+    systems = ["x86_64-linux" "aarch64-linux"];
+    crossSystemConfig = "aarch64-unknown-linux-musl";
+    ipupdDevShell = nixpkgs: with nixpkgs; mkShell {
       nativeBuildInputs = with pkgsBuildHost; [
         cargo
         rust-analyzer
@@ -21,5 +15,12 @@
       CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${stdenv.cc.targetPrefix}cc";
       CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER = "${stdenv.cc.targetPrefix}cc";
     };
+  in
+  {
+    devShells = nixpkgs.lib.genAttrs systems (system: 
+    {
+      default = ipupdDevShell (import nixpkgs { inherit system; });
+      ipupdCross = ipupdDevShell (import nixpkgs { inherit system; crossSystem = { config = crossSystemConfig; }; });
+    });
   };
 }
