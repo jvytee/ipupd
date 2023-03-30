672ab7000c48e11b64b622e40dc1fd38b928c105
Julian Theis <julian.theis@posteo.de>
2023-03-30 17:08:57 +0200

    Successfully perform static linking

--- flake.nix
+++ flake.nix
@@ -2,12 +2,14 @@
   outputs = { self, nixpkgs }:
   let
     system = "x86_64-linux";
-    crossSystem = { config = "aarch64-unknown-linux-gnu"; };
+    # crossSystem = { config = "aarch64-unknown-linux-gnu"; };
+    # crossSystem = { config = "aarch64-unknown-linux-musl"; };
+    crossSystem = { config = "x86_64-unknown-linux-musl"; };
     nixpkgsCross = import nixpkgs { inherit system crossSystem; };
   in
   with nixpkgsCross;
   {
-    devShells.${system}.default = mkShell {
+    devShells.${system}.ipupdAarch64 = mkShell {
       nativeBuildInputs = with pkgsBuildHost; [
         cargo
         rust-analyzer
@@ -17,6 +19,7 @@
       ];
 
       CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${stdenv.cc.targetPrefix}cc";
+      CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER = "${stdenv.cc.targetPrefix}cc";
     };
   };
 }
