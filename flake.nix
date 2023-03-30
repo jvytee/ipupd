2e3276af40d4c929970ef0cd7901c15ad3d78b2c
Julian Theis <julian.theis@posteo.de>
2023-03-30 16:29:56 +0200

    Adapt flake to enable cross compilation

--- flake.nix
+++ flake.nix
@@ -2,15 +2,21 @@
   outputs = { self, nixpkgs }:
   let
     system = "x86_64-linux";
+    crossSystem = { config = "aarch64-unknown-linux-gnu"; };
+    nixpkgsCross = import nixpkgs { inherit system crossSystem; };
   in
+  with nixpkgsCross;
   {
-    devShells.${system}.default = with import nixpkgs { inherit system; }; mkShell {
-      buildInputs = [
+    devShells.${system}.default = mkShell {
+      nativeBuildInputs = with pkgsBuildHost; [
         cargo
         rust-analyzer
         rustc
         rustfmt
+        stdenv.cc
       ];
+
+      CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${stdenv.cc.targetPrefix}cc";
     };
   };
 }
