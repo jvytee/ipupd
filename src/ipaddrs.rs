4d85ea02877af0caf157eae06b12a7cde8815223
Julian Theis <julian.theis@posteo.de>
2023-04-11 22:48:04 +0200

    Simplify fuctions to get ip addresses

--- src/ipaddrs.rs
+++ src/ipaddrs.rs
@@ -1,6 +1,7 @@
+use anyhow::{Context, Result};
 use pnet::datalink::interfaces;
 use pnet::ipnetwork::IpNetwork;
-use std::net::{SocketAddr, ToSocketAddrs};
+use std::net::{IpAddr, SocketAddr, ToSocketAddrs};
 
 #[derive(Debug)]
 pub struct IpAddrs {
@@ -52,7 +53,7 @@ impl IpAddrs {
                 if Self::is_global(&ip_network) {
                     match ip_network {
                         IpNetwork::V4(v4_network) => ip_addr.v4 = Some(v4_network.ip().to_string()),
-                        IpNetwork::V6(v6_network) => ip_addr.v6 = Some(v6_network.ip().to_string())
+                        IpNetwork::V6(v6_network) => ip_addr.v6 = Some(v6_network.ip().to_string()),
                     }
                 }
             }
@@ -64,18 +65,17 @@ impl IpAddrs {
     }
 
     pub fn v4_string(&self) -> String {
-        self.v4.clone()
-            .unwrap_or("0.0.0.0".to_string())
+        self.v4.clone().unwrap_or("0.0.0.0".to_string())
     }
 
     pub fn v6_string(&self) -> String {
-        self.v6.clone()
-            .unwrap_or("::".to_string())
+        self.v6.clone().unwrap_or("::".to_string())
     }
 
     fn is_global(ip_network: &IpNetwork) -> bool {
         match ip_network {
-            IpNetwork::V6(v6_network) => v6_network.ip()
+            IpNetwork::V6(v6_network) => v6_network
+                .ip()
                 .segments()
                 .first()
                 .map_or(false, |segment| 0x0000 < *segment && *segment < 0xf000),
@@ -84,5 +84,33 @@ impl IpAddrs {
                 !(ip.is_loopback() || ip.is_private() || ip.is_link_local())
             }
         }
+    }
+}
+
+pub fn domain_ipaddrs(domain_name: &str) -> Result<Vec<IpAddr>> {
+    let socket_addrs = format!("{}:443", domain_name)
+        .to_socket_addrs()
+        .with_context(|| format!("Could not resolve {}", domain_name))?;
+
+    Ok(socket_addrs.map(|socket_addr| socket_addr.ip()).collect())
+}
+
+pub fn interface_ipaddrs(if_name: &str) -> Vec<IpAddr> {
+    interfaces()
+        .iter()
+        .filter(|interface| interface.name == if_name)
+        .flat_map(|interface| interface.ips.clone())
+        .map(|network| network.ip())
+        .filter(is_global)
+        .collect()
+}
+
+fn is_global(ip_addr: &IpAddr) -> bool {
+    match ip_addr {
+        IpAddr::V6(ipv6) => ipv6
+            .segments()
+            .first()
+            .map_or(false, |segment| 0x0000 < *segment && *segment < 0xf000),
+        IpAddr::V4(ipv4) => !(ipv4.is_loopback() || ipv4.is_private() || ipv4.is_link_local()),
     }
 }
