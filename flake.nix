16289e5e8d07b5c5a5e802bdada23d03706018d3
Julian Theis <julian.theis@posteo.de>
2023-04-25 22:52:31 +0200

    Define ipupd packages

--- flake.nix
+++ flake.nix
@@ -1,27 +1,61 @@
 {
   outputs = { self, nixpkgs }:
-  let
-    systems = ["x86_64-linux" "aarch64-linux"];
-    crossSystemConfig = "aarch64-unknown-linux-musl";
-    ipupdDevShell = nixpkgs: with nixpkgs; mkShell {
-      nativeBuildInputs = with pkgsBuildHost; [
-        cargo
-        clippy
-        rust-analyzer
-        rustc
-        rustfmt
-        stdenv.cc
-      ];
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
 
-      CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${stdenv.cc.targetPrefix}cc";
-      CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER = "${stdenv.cc.targetPrefix}cc";
-    };
-  in
-  {
-    devShells = nixpkgs.lib.genAttrs systems (system: 
+      ipupdDevShell = nixpkgs: with nixpkgs; mkShell {
+        nativeBuildInputs = with pkgsBuildHost; [
+          cargo
+          clippy
+          nixpkgs-fmt
+          rust-analyzer
+          rustc
+          rustfmt
+          stdenv.cc
+        ];
+
+        CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${stdenv.cc.targetPrefix}cc";
+        # CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER = "${stdenv.cc.targetPrefix}cc";
+      };
+
+      ipupdPackage = nixpkgs: with nixpkgs; rustPlatform.buildRustPackage {
+        pname = "ipupd";
+        version = "0.3.0";
+
+        src = fetchgit {
+          url = "https://github.com/jvytee/ipupd.git";
+          rev = "e5b846416eabd43899b68f73880a694aca29f7fc";
+          sha256 = "sha256-s+zccrbBUatKT9Zbf/rKAZEtdVc7h99vZvQqm9Ri73Q=";
+        };
+
+        cargoLock.lockFile = ./Cargo.lock;
+
+        nativeBuildInputs = with pkgsBuildHost; [
+          rustc
+          cargo
+          stdenv.cc
+        ];
+
+        CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${stdenv.cc.targetPrefix}cc";
+      };
+    in
     {
-      default = ipupdDevShell (import nixpkgs { inherit system; });
-      ipupdCross = ipupdDevShell (import nixpkgs { inherit system; crossSystem = { config = crossSystemConfig; }; });
-    });
-  };
+      devShells = nixpkgs.lib.genAttrs systems (
+        system: {
+          default = ipupdDevShell (import nixpkgs { inherit system; });
+          ipupd-aarch64 = ipupdDevShell (import nixpkgs { inherit system; crossSystem.system = "aarch64-linux"; });
+        }
+      );
+
+      formatter = nixpkgs.lib.genAttrs systems (
+        system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt
+      );
+
+      packages = nixpkgs.lib.genAttrs systems (
+        system: {
+          default = ipupdPackage (import nixpkgs { inherit system; });
+          ipupd-aarch64 = ipupdPackage (import nixpkgs { inherit system; crossSystem.system = "aarch64-linux"; });
+        }
+      );
+    };
 }
