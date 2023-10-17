87e0f00c6acc8bd010495236239377cc41e2f39e
Julian Theis <julian.theis@posteo.de>
2023-10-17 22:51:16 +0200

    Finally build a statically linked binary

--- flake.nix
+++ flake.nix
@@ -10,36 +10,30 @@
   outputs = { self, nixpkgs, fenix }:
     let
       systems = [ "x86_64-linux" "aarch64-linux" ];
-      toolchain = fenix: system: target:
+      toolchain = fenix: system: rustTarget:
         with fenix.packages.${system};
-        if target == system
-        then
-          combine [
-            stable.cargo
-            stable.rustc
-            targets.x86_64-unknown-linux-musl.stable.rust-std
-          ]
-        else
-          combine [
-            stable.cargo
-            stable.rustc
-            targets.${target}.stable.rust-std
-          ];
+        combine [
+          stable.cargo
+          stable.rustc
+          targets.${rustTarget}.stable.rust-std
+        ];
 
-      importNixpkgs = nixpkgs: system: target:
+      importNixpkgs = system: target: rustTarget:
         if target == system
         then import nixpkgs { inherit system; }
         else import nixpkgs { inherit system; crossSystem.config = target; };
 
-      fenixRustPlatform = nixpkgs: fenix: system: target:
-        let fenixToolchain = toolchain fenix system target;
-        in nixpkgs.makeRustPlatform { cargo = fenixToolchain; rustc = fenixToolchain; };
+      fenixRustPlatform = pkgs: fenix: system: rustTarget:
+        let
+          fenixToolchain = toolchain fenix system rustTarget;
+          configuredStdenv = pkgs.stdenv.override (previous: { hostPlatform = previous.hostPlatform // { rustc.config = rustTarget; }; });
+        in pkgs.makeRustPlatform { cargo = fenixToolchain; rustc = fenixToolchain; stdenv = configuredStdenv; };
 
-      ipupdDevShell = nixpkgs: fenix: system: target:
-        with importNixpkgs nixpkgs system target;
+      ipupdDevShell = fenix: system: target: rustTarget:
+        with importNixpkgs system target rustTarget;
         mkShell {
           nativeBuildInputs = [
-            (toolchain fenix system target)
+            (toolchain fenix system rustTarget)
             gh
             rust-analyzer
             yaml-language-server
@@ -47,11 +41,11 @@
           CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${stdenv.cc.targetPrefix}cc";
         };
 
-      ipupdPackage = nixpkgs: fenix: system: target:
-        let pkgs = importNixpkgs nixpkgs system target;
+      ipupdPackage = fenix: system: target: rustTarget:
+        let pkgs = importNixpkgs system target rustTarget;
         in
         with pkgs;
-        (fenixRustPlatform pkgs fenix system target).buildRustPackage {
+        (fenixRustPlatform pkgs fenix system rustTarget).buildRustPackage {
           pname = "ipupd";
           version = "0.3.0";
           src = self;
@@ -65,8 +59,8 @@
     {
       devShells = nixpkgs.lib.genAttrs systems (
         system: {
-          default = ipupdDevShell nixpkgs fenix system system;
-          ipupd-aarch64 = ipupdDevShell nixpkgs fenix system "aarch64-unknown-linux-gnu";
+          default = ipupdDevShell fenix system system "x86_64-unknown-linux-musl";
+          ipupd-aarch64 = ipupdDevShell fenix system "aarch64-unknown-linux-gnu" "aarch64-unknown-linux-musl";
         }
       );
 
@@ -76,8 +70,8 @@
 
       packages = nixpkgs.lib.genAttrs systems (
         system: {
-          default = ipupdPackage nixpkgs fenix system system;
-          ipupd-aarch64 = ipupdPackage nixpkgs fenix system "aarch64-unknown-linux-gnu";
+          default = ipupdPackage fenix system system "x86_64-unknown-linux-musl";
+          ipupd-aarch64 = ipupdPackage fenix system "aarch64-unknown-linux-gnu" "aarch64-unknown-linux-musl";
         }
       );
     };
