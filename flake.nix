ccd236629e2b0227cc40235a7b582bc7997c055d
Julian Theis <julian.theis@posteo.de>
2023-11-06 22:00:44 +0100

    Drop cross compilation capabilities

--- flake.nix
+++ flake.nix
@@ -9,8 +9,7 @@
 
   outputs = { self, nixpkgs, fenix }:
     let
-      systems = [ "x86_64-linux" "aarch64-linux" ];
-      toolchain = fenix: system: rustTarget:
+      toolchain = { system, rustTarget }:
         with fenix.packages.${system};
         combine [
           stable.cargo
@@ -18,61 +17,47 @@
           targets.${rustTarget}.stable.rust-std
         ];
 
-      importNixpkgs = system: target: rustTarget:
-        if target == system
-        then import nixpkgs { inherit system; }
-        else import nixpkgs { inherit system; crossSystem.config = target; };
-
-      fenixRustPlatform = pkgs: fenix: system: rustTarget:
+      rustPlatform = { pkgs, system, rustTarget }:
         let
-          fenixToolchain = toolchain fenix system rustTarget;
-          configuredStdenv = pkgs.stdenv.override (previous: { hostPlatform = previous.hostPlatform // { rustc.config = rustTarget; }; });
+          fenixToolchain = toolchain { inherit system rustTarget; };
+          configuredStdenv = pkgs.stdenv.override (prev: { hostPlatform = prev.hostPlatform // { rustc.config = rustTarget; }; });
         in pkgs.makeRustPlatform { cargo = fenixToolchain; rustc = fenixToolchain; stdenv = configuredStdenv; };
 
-      ipupdDevShell = fenix: system: target: rustTarget:
-        with importNixpkgs system target rustTarget;
-        mkShell {
-          nativeBuildInputs = [
-            (toolchain fenix system rustTarget)
+      ipupdDevShell = { system, rustTarget ? system }:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          fenixToolchain = toolchain { inherit system rustTarget; };
+        in pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            fenixToolchain
             gh
             rust-analyzer
             yaml-language-server
           ];
-          CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${stdenv.cc.targetPrefix}cc";
         };
 
-      ipupdPackage = fenix: system: target: rustTarget:
-        let pkgs = importNixpkgs system target rustTarget;
-        in
-        with pkgs;
-        (fenixRustPlatform pkgs fenix system rustTarget).buildRustPackage {
+      ipupdPackage = { system, rustTarget ? system }:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          fenixRustPlatform = rustPlatform { inherit pkgs system rustTarget; };
+        in fenixRustPlatform.buildRustPackage {
           pname = "ipupd";
           version = "0.3.0";
           src = self;
 
           cargoLock.lockFile = ./Cargo.lock;
-          depsBuildTarget = with pkgsBuildTarget; [ stdenv.cc ];
-
-          CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${stdenv.cc.targetPrefix}cc";
         };
-    in
-    {
-      devShells = nixpkgs.lib.genAttrs systems (
-        system: {
-          default = ipupdDevShell fenix system system "x86_64-unknown-linux-musl";
-          ipupd-aarch64 = ipupdDevShell fenix system "aarch64-unknown-linux-gnu" "aarch64-unknown-linux-musl";
-        }
-      );
+    in {
+      devShells = {
+        x86_64-linux.default = ipupdDevShell { system = "x86_64-linux"; rustTarget = "x86_64-unknown-linux-musl"; };
+        aarch64-linux.default = ipupdDevShell { system = "aarch64-linux"; rustTarget = "aarch64-unknown-linux-musl"; };
+      };
 
-      formatter = nixpkgs.lib.genAttrs systems (
-        system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt
-      );
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
 
-      packages = nixpkgs.lib.genAttrs systems (
-        system: {
-          default = ipupdPackage fenix system system "x86_64-unknown-linux-musl";
-          ipupd-aarch64 = ipupdPackage fenix system "aarch64-unknown-linux-gnu" "aarch64-unknown-linux-musl";
-        }
-      );
+      packages = {
+        x86_64-linux.default = ipupdPackage { system = "x86_64-linux"; };
+        aarch64-linux.default = ipupdPackage { system = "aarch64-linux"; };
+      };
     };
 }
