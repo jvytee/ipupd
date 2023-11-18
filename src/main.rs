e3c44a62021f7481d75b7cf820b6b62467fae380
Julian Theis <julian.theis@posteo.de>
2023-11-18 23:21:09 +0100

    Use env_logger

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,17 @@ use anyhow::{Context, Result};
 use config::Config;
 use getopts::Options;
 use ipaddrs::IpAddrs;
+use log;
 use std::{collections::HashSet, env, net::IpAddr, process};
 use ureq::Request;
 
 const DEFAULT_CONFIG: &str = "/etc/ipupd/config.toml";
 
 fn main() {
+    env_logger::init();
+
     if let Err(error) = try_main() {
-        eprintln!("{}", error);
+        log::error!("{error}");
         process::exit(1);
     }
 }
@@ -37,15 +40,19 @@ fn try_main() -> Result<()> {
     }
 
     let config_file = matches.opt_str("c").unwrap_or(DEFAULT_CONFIG.to_string());
+    log::info!("Loading config file {config_file}");
     let config = Config::from_file(&config_file)
-        .with_context(|| format!("Could not parse config file {}", &config_file))?;
+        .with_context(|| format!("Could not load config file {config_file}"))?;
 
     let interface = &config.interface;
+    log::info!("Reading IPv6s of interface {interface}");
     let interface_ips = IpAddrs::from_interface(interface);
 
     let ip_addrs = match &config.api {
         Some(endpoint) => {
-            let api_ips = IpAddrs::from_api(endpoint)?;
+            log::info!("Requesting IPv4s from API {endpoint}");
+            let api_ips = IpAddrs::from_api(endpoint)
+                .with_context(|| format!("Could not request IPs from {endpoint}"))?;
             IpAddrs(
                 interface_ips
                     .union(&api_ips)
@@ -57,18 +64,21 @@ fn try_main() -> Result<()> {
     };
 
     let domain = &config.domain;
+    log::info!("Resolving IPs of domain {domain}");
     let domain_ips =
-        IpAddrs::from_domain(domain).with_context(|| format!("Could not resolve {}", domain))?;
+        IpAddrs::from_domain(domain).with_context(|| format!("Could not resolve {domain}"))?;
 
     if !ip_addrs.is_subset(&domain_ips) {
+        log::info!("Sending IPs to {}", &config.url);
         let request = create_request(&config, &ip_addrs);
-        let response = request.call().context("Could not perform GET request")?;
-        println!(
-            "{}",
-            response
-                .into_string()
-                .context("Could not read response body")?
-        );
+        let response = request
+            .call()
+            .with_context(|| format!("Could not GET {}", &config.url))?;
+        let status = response.status();
+        let status_text = response.status_text().to_string();
+        log::info!("Got {status} {status_text}. Done.");
+    } else {
+        log::info!("IPs are up to date. Done.");
     }
 
     Ok(())
