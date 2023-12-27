598433f6dd73d431cfd9a9e558488922ecb7094b
Julian Theis <julian.theis@posteo.de>
2023-12-27 23:52:20 +0100

    Follow NixOS 23.11 for nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -9,35 +9,25 @@
 
   outputs = { self, nixpkgs, fenix }:
     let
-      toolchain = { system, rustTarget }:
+      toolchain = system:
         with fenix.packages.${system};
-        if rustTarget == system
-        then combine [
+        combine [
           stable.cargo
           stable.rustc
-        ]
-        else combine [
-          stable.cargo
-          stable.rustc
-          targets.${rustTarget}.stable.rust-std
         ];
 
       devToolchain = system: fenix.packages.${system}.stable.toolchain;
 
-      rustPlatform = { pkgs, system, rustTarget }:
-        let
-          fenixToolchain = toolchain { inherit system rustTarget; };
-          configuredStdenv = pkgs.stdenv.override (prev: { hostPlatform = prev.hostPlatform // { rustc.config = rustTarget; }; });
-        in
-          if rustTarget == system
-          then pkgs.makeRustPlatform { cargo = fenixToolchain; rustc = fenixToolchain; }
-          else pkgs.makeRustPlatform { cargo = fenixToolchain; rustc = fenixToolchain; stdenv = configuredStdenv; };
+      rustPlatform = { pkgs, system }:
+        let fenixToolchain = toolchain system;
+        in pkgs.makeRustPlatform { cargo = fenixToolchain; rustc = fenixToolchain; };
 
-      ipupdDevShell = { system, rustTarget ? system }:
+      ipupdDevShell = system:
         let
           pkgs = import nixpkgs { inherit system; };
           fenixToolchain = devToolchain system;
-        in pkgs.mkShell {
+        in
+        pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             fenixToolchain
             gh
@@ -45,30 +35,30 @@
           ];
         };
 
-      ipupdPackage = { system, rustTarget ? system }:
+      ipupdPackage = system:
         let
           pkgs = import nixpkgs { inherit system; };
-          fenixRustPlatform = rustPlatform { inherit pkgs system rustTarget; };
-        in fenixRustPlatform.buildRustPackage {
+          fenixRustPlatform = rustPlatform { inherit pkgs system; };
+        in
+        fenixRustPlatform.buildRustPackage {
           pname = "ipupd";
           version = "0.3.0";
           src = self;
 
           cargoLock.lockFile = ./Cargo.lock;
         };
-    in {
+    in
+    {
       devShells = {
-        x86_64-linux.default = ipupdDevShell { system = "x86_64-linux"; rustTarget = "x86_64-unknown-linux-musl"; };
-        aarch64-linux.default = ipupdDevShell { system = "aarch64-linux"; rustTarget = "aarch64-unknown-linux-musl"; };
+        x86_64-linux.default = ipupdDevShell "x86_64-linux";
+        aarch64-linux.default = ipupdDevShell "aarch64-linux";
       };
 
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
 
       packages = {
-        x86_64-linux.default = ipupdPackage { system = "x86_64-linux"; };
-        x86_64-linux.static = ipupdPackage { system = "x86_64-linux"; rustTarget = "x86_64-unknown-linux-musl"; };
-        aarch64-linux.default = ipupdPackage { system = "aarch64-linux"; };
-        aarch64-linux.static = ipupdPackage { system = "aarch64-linux"; rustTarget = "aarch64-unknown-linux-musl"; };
+        x86_64-linux.default = ipupdPackage "x86_64-linux";
+        aarch64-linux.default = ipupdPackage "aarch64-linux";
       };
     };
 }
